9296286431231b3642042dcb4965c08d20b66bb7
Alexander Martyushev <you@example.com>
2023-10-15 22:27:06 +0300

    Created LogGlobalPreFilter

---
+++ src/main/java/org/gateway/LogGlobalPreFilter.java
@@ -0,0 +1,21 @@
+package org.gateway;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class LogGlobalPreFilter implements GlobalFilter {
+
+    final Logger log = LoggerFactory.getLogger(LogGlobalPreFilter.class);
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("LogGlobalPreFilter executed");
+        return chain.filter(exchange);
+    }
+}
